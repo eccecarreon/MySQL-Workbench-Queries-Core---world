8750310f50a00926a22e456e28bb1bc36b5858db
eccecarreon <153371886+eccecarreon@users.noreply.github.com>
2024-03-20 21:47:23 +0800

    Initial commit

---
+++ world_query2.sql
@@ -0,0 +1,44 @@
+USE world;
+SHOW tables;
+
+SELECT *
+FROM cities;
+
+SELECT *
+FROM countries;
+
+SELECT continent, COUNT(continent) AS 'number of countries'
+FROM countries
+GROUP BY continent
+ORDER BY COUNT(continent) DESC;
+
+SELECT countries.name, languages.language, languages.percentage 
+FROM languages
+JOIN countries ON countries.id = languages.country_id
+WHERE languages.language = "Greek"
+ORDER BY languages.percentage DESC;
+
+SELECT name, surface_area, population
+FROM countries
+WHERE surface_area<1000 OR population>1000000000;
+
+SELECT name, government_form, life_expectancy
+FROM countries
+WHERE government_form="Federal Republic" AND life_expectancy<75;
+
+SELECT countries.name, cities.name, cities.district, cities.population
+FROM countries
+JOIN cities ON countries.id = cities.country_id
+WHERE countries.name="Mexico" AND cities.district="Veracruz" AND cities.population>100000;
+
+SELECT countries.name, cities.name, cities.population
+FROM countries
+JOIN cities ON countries.id = cities.country_id
+WHERE countries.name ="Brazil"
+ORDER BY cities.population ASC
+LIMIT 10;
+
+SELECT countries.name, languages.language, languages.percentage 
+FROM languages
+JOIN countries ON countries.id = languages.country_id
+WHERE countries.name="Monaco" AND languages.percentage>10;
